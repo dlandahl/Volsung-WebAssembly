29262460339758146476de44208752d9e7ccb255
long-march <landahl2014@gmail.com>
2020-06-25 00:58:21 +0100

    Copy example programs from language repo

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ echo "Sourcing Emscripten"
 source ../emsdk/emsdk_env.sh
 
 echo "Building"
-emcc --bind main.cc Volsung/src/*.cc -I Volsung/include/ -std=c++17 -O3 -s DISABLE_EXCEPTION_CATCHING=0 --preload-file Volsung/test/test_programs@/ --preload-file Volsung/extra/standard_library@/ -s ALLOW_MEMORY_GROWTH=1
+#emcc --bind main.cc Volsung/src/*.cc -I Volsung/include/ -std=c++17 -O3 -s DISABLE_EXCEPTION_CATCHING=0 --preload-file Volsung/test/test_programs@/ --preload-file Volsung/extra/standard_library@/ -s ALLOW_MEMORY_GROWTH=1
 
 echo "Moving"
 mv a.out.* website
+cp Volsung/test/test_programs/*.vlsng website/res/test_programs/
